6f43713dc8533f074455fa99732dde6933287bf7
Greg Horn <gregmainland@gmail.com>
2013-02-27 22:52:01 +0100

    tweak plotter to work with multiple channels

--- src/Main.hs
+++ src/Main.hs
@@ -20,22 +20,27 @@ import qualified Text.ProtocolBuffers as PB
 --import qualified System.Remote.Monitoring as EKG
 
 import qualified Kite.MultiCarousel as MC
+import qualified Kite.CarouselState as CS
 
 import ParseArgs ( getip )
 import Plotter ( runPlotter, newChannel, makeAccessors )
+import PlotTypes ( Channel(..) )
 
 main :: IO ()
 main = do
 --  ekgTid <- fmap EKG.serverThreadId $ EKG.forkServer "localhost" 8000
-  ip <- getip "multicarousel" "tcp://localhost:5563"
+  ip <- getip "plot-ho-matic" "tcp://localhost:5563"
   putStrLn $ "using ip \""++ip++"\""
   
-  (c0, chan0) <- newChannel "carousel" $(makeAccessors ''MC.MultiCarousel)
-  listenerTid <- CC.forkIO (sub ip chan0)
+  (c0, chan0) <- newChannel "multi-carousel" $(makeAccessors ''MC.MultiCarousel)
+  (c1, chan1) <- newChannel "carousel" $(makeAccessors ''CS.CarouselState)
+  listenerTid0 <- CC.forkIO (sub ip chan0 c0)
+  listenerTid1 <- CC.forkIO (sub ip chan1 c1)
   
-  runPlotter [c0] [ listenerTid
+  runPlotter [c0,c1] [ listenerTid0
+                     , listenerTid1
 --                  , ekgTid
-                  ]
+                     ]
 
 withContext :: (ZMQ.Context -> IO a) -> IO a
 #if OSX
@@ -44,8 +49,8 @@ withContext = ZMQ.withContext
 withContext = ZMQ.withContext 1
 #endif
 
-sub :: (PB.Wire a, PB.ReflectDescriptor a) => String -> CC.Chan a -> IO ()
-sub ip chan = withContext $ \context -> do
+sub :: (PB.Wire a, PB.ReflectDescriptor a) => String -> CC.Chan a -> Channel -> IO ()
+sub ip chan channel = withContext $ \context -> do
 #if OSX
   let receive = ZMQ.receive
 #else
@@ -53,7 +58,7 @@ sub ip chan = withContext $ \context -> do
 #endif
   ZMQ.withSocket context ZMQ.Sub $ \subscriber -> do
     ZMQ.connect subscriber ip
-    ZMQ.subscribe subscriber "multi-carousel"
+    ZMQ.subscribe subscriber (chanName channel)
     forever $ do
       _ <- receive subscriber
       mre <- ZMQ.moreToReceive subscriber

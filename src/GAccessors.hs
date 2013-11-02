2439874a02094fade0b346e47f9014772b88742d
Greg Horn <gregmainland@gmail.com>
2013-11-02 18:15:38 +0100

    more restrictive tree for generic accessors

--- src/GAccessors.hs
+++ src/GAccessors.hs
@@ -1,37 +1,28 @@
 {-# OPTIONS_GHC -Wall #-}
-{-# OPTIONS_GHC -ddump-deriv #-}
 {-# LANGUAGE DefaultSignatures #-}
 {-# LANGUAGE TypeOperators #-}
 {-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE FlexibleInstances #-}
-{-# LANGUAGE DeriveGeneric #-}
+--{-# LANGUAGE DeriveGeneric #-}
 
 module GAccessors ( AccessorTree(..), accessors ) where
 
 import GHC.Generics
 
 showAccTree :: String -> AccessorTree a -> [String]
-showAccTree spaces (Getter _) = [spaces ++ "Getter"]
-showAccTree spaces (Data name tree) =
-  [spaces ++ "Data " ++ name] ++
-  showAccTree (spaces ++ "    ") tree
-showAccTree spaces (Constructor name tree) =
-  [spaces ++ "Constructor " ++ name] ++
-  showAccTree (spaces ++ "    ") tree
-showAccTree spaces (Selector name tree) =
-  [spaces ++ "Selector " ++ name] ++
-  showAccTree (spaces ++ "    ") tree
-showAccTree spaces (Product x y) =
-  [spaces ++ "Product"] ++
-  concatMap (showAccTree (spaces ++ "    ")) [x,y]
+showAccTree spaces (Getter _) = [spaces ++ "Getter {}"]
+showAccTree spaces (Data name trees) =
+  [spaces ++ "Data " ++ show name] ++
+  concatMap (showChild (spaces ++ "    ")) trees
+
+showChild :: String -> (String, AccessorTree a) -> [String]
+showChild spaces (name, tree) =
+  [spaces ++ name] ++ (showAccTree (spaces ++ "    ") tree)
 
 instance Show (AccessorTree a) where
   show = unlines . showAccTree ""
 
-data AccessorTree a = Data String (AccessorTree a)
-                    | Constructor String (AccessorTree a)
-                    | Product (AccessorTree a) (AccessorTree a)
-                    | Selector String (AccessorTree a)
+data AccessorTree a = Data (String,String) [(String, AccessorTree a)]
                     | Getter (a -> Double)
 
 accessors :: Lookup a => a -> AccessorTree a
@@ -46,6 +37,9 @@ class Lookup a where
 class GLookup f where
   gtoAccessorTree :: f a -> (b -> f a) -> AccessorTree b
 
+class GLookupS f where
+  gtoAccessorTreeS :: f a -> (b -> f a) -> [(String, AccessorTree b)]
+
 instance Lookup Float where
   toAccessorTree _ f = Getter $ realToFrac . f
 instance Lookup Double where
@@ -56,36 +50,35 @@ instance Lookup Int where
 instance (Lookup f, Generic f) => GLookup (Rec0 f) where
   gtoAccessorTree x f = toAccessorTree (unK1 x) (unK1 . f)
 
-instance (GLookup f, GLookup g) => GLookup (f :*: g) where
-  gtoAccessorTree (x :*: y) f = Product tf tg
+instance (Selector s, GLookup a) => GLookupS (S1 s a) where
+  gtoAccessorTreeS x f = [(selName x, gtoAccessorTree (unM1 x) (unM1 . f))]
+
+instance (GLookupS f, GLookupS g) => GLookupS (f :*: g) where
+  gtoAccessorTreeS (x :*: y) f = tf ++ tg
     where
-      tf = gtoAccessorTree x $ left . f
-      tg = gtoAccessorTree y $ right . f
+      tf = gtoAccessorTreeS x $ left . f
+      tg = gtoAccessorTreeS y $ right . f
 
       left  ( x' :*: _  ) = x'
       right ( _  :*: y' ) = y'
 
-instance (Selector s, GLookup a) => GLookup (S1 s a) where
-  gtoAccessorTree x f = Selector (selName x) $ gtoAccessorTree (unM1 x) (unM1 . f)
+instance (Datatype d, Constructor c, GLookupS a) => GLookup (D1 d (C1 c a)) where
+  gtoAccessorTree d@(M1 c) f = Data (datatypeName d, conName c) con
+    where
+      con = gtoAccessorTreeS (unM1 c) (unM1 . unM1 . f)
 
-instance (Constructor c, GLookup a) => GLookup (C1 c a) where
-  gtoAccessorTree x f = Constructor (conName x) $ gtoAccessorTree (unM1 x) (unM1 . f)
-
-instance (Datatype d, GLookup (C1 c a)) => GLookup (D1 d (C1 c a)) where
-  gtoAccessorTree x f = Data (datatypeName x) $ gtoAccessorTree (unM1 x) (unM1 . f)
-
-data Xyz = Xyz { xx :: Int
-               , yy :: Double
-               , zz :: Float
-               , ww :: Int
-               } deriving (Generic)
-data One = One { one :: Double } deriving (Generic)
-data Foo = Foo { aaa :: Int
-               , bbb :: Xyz
-               , ccc :: One
-               } deriving (Generic)
-instance Lookup One
-instance Lookup Xyz
-instance Lookup Foo
-foo :: Foo
-foo = Foo 2 (Xyz 6 7 8 9) (One 17)
+--data Xyz = Xyz { xx :: Int
+--               , yy :: Double
+--               , zz :: Float
+--               , ww :: Int
+--               } deriving (Generic)
+--data One = MkOne { one :: Double } deriving (Generic)
+--data Foo = Foo { aaa :: Int
+--               , bbb :: Xyz
+--               , ccc :: One
+--               } deriving (Generic)
+--instance Lookup One
+--instance Lookup Xyz
+--instance Lookup Foo
+--foo :: Foo
+--foo = Foo 2 (Xyz 6 7 8 9) (MkOne 17)

04f1f808b2e6f7f4578cf35c216e526b90f65342
Greg Horn <gregmainland@gmail.com>
2015-03-08 08:31:19 +0100

    bigger struct for example

--- examples/Example.hs
+++ examples/Example.hs
@@ -9,36 +9,39 @@ import GHC.Generics ( Generic )
 
 import PlotHo ( Lookup, XAxisType(..), runPlotter, addHistoryChannel )
 
-data Xyz = MkXyz { x :: Double
-                 , y :: Double
-                 , z :: Double
-                 } deriving Generic
+data Xyz a = MkXyz { x :: Double
+                   , y :: Double
+                   , z :: Double
+                   , zz :: a
+                   } deriving Generic
 data Axyz = MkAxyz { lol :: Double
 --                   , xyzList :: S.Seq Xyz
-                   , xyz :: Xyz
+                   , xyz1 :: Xyz (Xyz (Xyz Double))
+                   , xyz2 :: Xyz Double
                    } deriving Generic
-instance Lookup Xyz
+instance Lookup a => Lookup (Xyz a)
 instance Lookup Axyz
 
 axyz0 :: Axyz
 axyz0 = MkAxyz
         7
 --        (S.fromList [MkXyz 1 2 3])
-        (MkXyz 1 2 3)
+        (MkXyz 1 2 3 (MkXyz 4 5 6 (MkXyz 7 8 9 10)))
+        (MkXyz 1 2 3 4)
 
-xyz0 :: Xyz
-xyz0 = MkXyz 1 2 3
+xyz0 :: Xyz Double
+xyz0 = MkXyz 1 2 3 0.1
 
 incrementAxyz :: Axyz -> Axyz
-incrementAxyz (MkAxyz a _) = MkAxyz
+incrementAxyz (MkAxyz a _ _) = MkAxyz
                              (a+0.2)
---                             (S.take 5 (xyz <| xyzs))
-                             xyz'
+                             (xyz' (xyz' (xyz' (sin (3*a)))))
+                             (xyz' (sin (2*a)))
   where
-    xyz' = MkXyz (sin a) (cos a) (sin a * cos a)
+    xyz' w = MkXyz (sin a) (cos a) (sin a * cos a) w
 
-incrementXyz :: Xyz -> Xyz
-incrementXyz (MkXyz a _ _) = MkXyz (a+0.3) (2 * sin a) (3 * cos a)
+incrementXyz :: Xyz a -> Xyz a
+incrementXyz (MkXyz a _ _ b) = MkXyz (a+0.3) (2 * sin a) (3 * cos a) b
 
 -- a random function to write a bunch of data to a chan
 channelWriter :: Int -> Int -> (a -> a) -> a -> (a -> Bool -> IO ()) -> IO ()
